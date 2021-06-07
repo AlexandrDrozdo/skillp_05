11849cb926b668a55e78a6a0288c4645d65cb1bd
AlexandrDrozdov <drozdovalexandr75@gmail.com>
2021-06-07 17:16:34 +0300

    changes

---
+++ Lesson_3/lesson3.py
@@ -0,0 +1,56 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> abs(-99)
+99
+>>> ord('y')
+121
+>>> ord("^")
+94
+>>> pow (6,5)
+7776
+>>> str ('5')
+'5'
+>>> str (object='5')
+'5'
+>>> help
+Type help() for interactive help, or help(object) for help about object.
+>>> help (abs)
+Help on built-in function abs in module builtins:
+
+abs(x, /)
+    Return the absolute value of the argument.
+
+>>> ascii('#')
+"'#'"
+>>> ascii("#")
+"'#'"
+>>> bool ()
+False
+>>> bool (1)
+True
+>>> int(9.7)
+9
+>>> float ('23e-0000')
+23.0
+>>> float('25e-0002
+      
+SyntaxError: EOL while scanning string literal
+>>> float('25e-0002')
+0.25
+>>> print("asus")
+asus
+>>> print ('Ma name', 'Ssha')
+Ma name Ssha
+>>> print ('Ma name', 'Ssha', end=' ')
+Ma name Ssha 
+>>> chr (121)
+'y'
+>>> complex (1,2e-2)
+(1+0.02j)
+>>> complex(1)
+(1+0j)
+>>> round (4.798646,5)
+4.79865
+>>> round (4.9)
+5
+>>> 
